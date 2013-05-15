0001ad3543a2644feb5d0d4d765477cca477ca1e
nineoh <n_lanfranchi@hotmail.com>
2013-05-15 16:32:00 +0200

    add background image and implement localStorage for tasks

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,38 @@
-var activeTheme = '';
+var activeTheme;
+var taskArray;
 
 var initPage = function () {
 	// Load theme
 	activeTheme = localStorage['theme'] != null ? localStorage['theme'] : '#08C';
 	changeTheme(activeTheme);
 
-	// TODO: Load Tasks
-	
-}
+	// Load tasks
+	try {
+		loadAllTasks();
+		// taskArray = localStorage['taskArray'] != null ? JSON.parse(localStorage['taskArray']) : new Array();
+		// debugger;
+		// for (var i = 0; i < taskArray.length; i++) {
+		// 	$('#taskList').prepend(taskArray[i]);
+		// }
+	} catch(ex) {
+		alert('loading failed');
+	}
+};
+
+var loadAllTasks = function () {
+	$('#taskList').html(localStorage['taskArray']);
+};
+
+var storeAllTasks = function () {
+	localStorage['taskArray'] = $('#taskList').html();
+};
 
 var showCreateTaskBar = function () {
 	$(this).children().slideDown(300, function() {
 		$(this).fadeTo(300, 1.0);
 		$(this).stop();
 	});
-}
+};
 
 var hideCreateTaskBar = function () {
 	if ($('div#subHeader2').children().css('opacity') == 0.0)
@@ -25,7 +43,7 @@ var hideCreateTaskBar = function () {
 			$(this).stop();
 		});
 	});
-}
+};
 
 var highlightRow = function () {
 	//$(this).addClass('selected');
@@ -80,9 +98,13 @@ var createNewTask = function () {
 	var date = $('#txtDate').val();
 	var task = $('#txtTask').val();
 
-	$('#taskList').prepend('<div class="row-fluid task"><div class="span1">' + prio + '</div><div class="span2">' + date + '</div><div class="span2 taskText">' + task + '</div><div class="span1 offset5 doneButton hiddenButton"><input type="button" /></div><div class="span1 deleteButton hiddenButton"><input type="button" /></div></div>');
+	var taskString = '<div class="row-fluid task"><div class="span1">' + prio + '</div><div class="span2">' + date + '</div><div class="span2 taskText">' + task + '</div><div class="span1 offset5 doneButton hiddenButton"><input type="button" /></div><div class="span1 deleteButton hiddenButton"><input type="button" /></div></div>';
+	$('#taskList').prepend(taskString);
 
-	// TODO: store task into localStorage
+	// Store task into localStorage
+	// taskArray.push(taskString);
+	// localStorage['taskArray'] = JSON.stringify(taskArray);
+	storeAllTasks();
 }
 
 var clearInputFields = function () {
@@ -108,12 +130,12 @@ var storeUserData = function () {
 	else {
 		// no native support for HTML5 storage
 		alert('Your browser is NOT able to store your data!');
-   }
+	}
 }
 
 var createMockTasks = function () {
 	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		
+
 	for (var i = 0; i < 30; i++) {
 		var text = '';
 		var len = Math.floor((Math.random()*20)+1); 
@@ -126,6 +148,21 @@ var createMockTasks = function () {
 	}
 };
 
+var parseTaskList = function () {
+	var tasks = new Array();
+	var list = $('#taskList').children().each(function () {
+		tasks.push(String($(this)));
+	});
+
+	return tasks;
+};
+
+var storeTasks = function () {
+	var tasks = parseTaskList();
+	// localStorage['taskArray'] = JSON.stringify(tasks);
+	localStorage['taskArray'] = JSON.stringify($('#taskList').html());
+};
+
 
 $(function() {
 	// Initialize Page
@@ -134,74 +171,77 @@ $(function() {
 	/*
 	 * DatePicker configuration and activation.
 	 */
-	$.datepicker.regional['de-CH'] = {
-		closeText: 'schliessen',
-		prevText: '&#x3c;zurück',
-		nextText: 'nächster&#x3e;',
-		currentText: 'heute',
-		monthNames: ['Januar','Februar','März','April','Mai','Juni', 'Juli','August','September','Oktober','November','Dezember'],
-		monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun','Jul','Aug','Sep','Okt','Nov','Dez'],
-		dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
-		dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
-		dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
-		weekHeader: 'Wo',
-		dateFormat: 'dd.mm.yy',
-		firstDay: 1,
-		isRTL: false,
-		showMonthAfterYear: false,
-		yearSuffix: ''
-	};
-	$.datepicker.setDefaults($.datepicker.regional['de-CH']);
-	$('#txtDate').datepicker();
+	 $.datepicker.regional['de-CH'] = {
+	 	closeText: 'schliessen',
+	 	prevText: '&#x3c;zurück',
+	 	nextText: 'nächster&#x3e;',
+	 	currentText: 'heute',
+	 	monthNames: ['Januar','Februar','März','April','Mai','Juni', 'Juli','August','September','Oktober','November','Dezember'],
+	 	monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun','Jul','Aug','Sep','Okt','Nov','Dez'],
+	 	dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+	 	dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+	 	dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+	 	weekHeader: 'Wo',
+	 	dateFormat: 'dd.mm.yy',
+	 	firstDay: 1,
+	 	isRTL: false,
+	 	showMonthAfterYear: false,
+	 	yearSuffix: ''
+	 };
+	 $.datepicker.setDefaults($.datepicker.regional['de-CH']);
+	 $('#txtDate').datepicker();
 
-	$('div#subHeader2').hover(showCreateTaskBar);
-	$('div#content').click(hideCreateTaskBar);
-	$('div#taskList').on('mouseover', 'div.task', highlightRow);
-	$('div#taskList').on('mouseleave', 'div.task', unhighlightRow);
-	$('div#footer').click(createMockTasks);
-	$('div#themeSelection div.blueTheme').click(function () {
-		changeTheme('#08C');
-	});
-	$('div#themeSelection div.pinkTheme').click(function () {
-		changeTheme('#F0F');
-	});
-	$('div#themeSelection div.greenTheme').click(function () {
-		changeTheme('#0F0');
-	});
-	$('div#themeSelection div').hover(function () {
-		$(this).css('cursor', 'pointer');
-		$(this).css('box-shadow', '0 0 10px 10px ' + $(this).css('background-color'));
-	}, function () {
-		$(this).css('box-shadow', 'none');
-	});
+	 $('div#subHeader2').hover(showCreateTaskBar);
+	 $('div#content').click(hideCreateTaskBar);
+	 $('div#taskList').on('mouseover', 'div.task', highlightRow);
+	 $('div#taskList').on('mouseleave', 'div.task', unhighlightRow);
+	 $('div#footer').click(createMockTasks);
+	 $('div#themeSelection div.blueTheme').click(function () {
+	 	changeTheme('#08C');
+	 });
+	 $('div#themeSelection div.pinkTheme').click(function () {
+	 	changeTheme('#F0F');
+	 });
+	 $('div#themeSelection div.greenTheme').click(function () {
+	 	changeTheme('#0F0');
+	 });
+	 $('div#themeSelection div').hover(function () {
+	 	$(this).css('cursor', 'pointer');
+	 	$(this).css('box-shadow', '0 0 10px 10px ' + $(this).css('background-color'));
+	 }, function () {
+	 	$(this).css('box-shadow', 'none');
+	 });
 
 	/*
 	 * Create task on button click
 	 */
-	$('#btnCreate').click(function () {
-		if (!isValid())
-			return;
+	 $('#btnCreate').click(function () {
+	 	if (!isValid())
+	 		return;
 
-		createNewTask();
-		clearInputFields();
-	});
+	 	createNewTask();
+	 	clearInputFields();
+	 });
 
 	/*
 	 * Mark tas as done
 	 */
-	$('#taskList').on('click', '.doneButton', function () {
-		$(this).parent().toggleClass('done');
-	});
+	 $('#taskList').on('click', '.doneButton', function () {
+	 	$(this).parent().toggleClass('done');
+	 	storeAllTasks();
+	 });
 
 	/*
 	 * Delete button click
 	 */
-	$('#taskList').on('click', '.deleteButton', function () {
-		$(this).parent().remove();
+	 $('#taskList').on('click', '.deleteButton', function () {
+	 	$(this).parent().remove();
+	 	// storeTasks();
+	 	storeAllTasks();
 	});
 
-	$('div.hiddenRow').children().children('select, input').change(function () {
-		$(this).removeClass('errorFocus');
-	});
+	 $('div.hiddenRow').children().children('select, input').change(function () {
+	 	$(this).removeClass('errorFocus');
+	 });
 
-});
+	});
